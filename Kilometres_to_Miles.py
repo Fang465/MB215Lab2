72ce8dbe3d9731dfeb42ed469e617a589d2e521b
Fang465 <kita5460@mylaurier.ca>
2022-01-13 11:10:28 -0500

    Minor script change

--- Kilometres_to_Miles.py
+++ Kilometres_to_Miles.py
@@ -1,6 +1,5 @@
 print ("This converter will convert kilometres to miles.")
 kilometres = float(input("Enter distance in kilometres "))
 miles = kilometres*0.62137
-kilometres = str(kilometres)
-miles = str(round(miles, 2))
-print (kilometres + " kilometres is " + miles + " miles")
+miles = round(miles, 2)
+print (kilometres, " kilometres is ", miles, " miles")
